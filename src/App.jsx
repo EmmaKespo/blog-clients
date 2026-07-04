06ba058686ae81850c2f34afe48e13c8b3bf7ec3
EmmaKespo <kespoemma@gmail.com>
2026-07-04 11:15:51 +0100

    wire postdetails and blogfeed

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,23 @@ export default function App(){
 
     return(
         <div className="min-h-screen  bg-slate-950 text-slate-100 p-4 sm:p-6 lg:p-8 font-sans">
-            
+           <header className="max-w-2xl mx-auto text-center mb-8"> 
+               <h1 className="text-3xl font-bold">Blog</h1>
+                <p className="text-slate-400 text-sm mt-2">Welcome to my blog!</p>
+           </header>
+        <main>
+          {selectedPost ? (
+            <PostDetails post={selectedPost} onBack={() => setSelectedPost(null)} />
+          ) : (
+            <BlogFeed posts={posts} onSelectClick={(post) => setSelectedPost(post)} />
+          )}
 
-     
+        </main>
+
+
+
+
+
         </div>
-    )
+    );
 }
